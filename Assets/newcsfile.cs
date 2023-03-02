341dee23d527fe9ddb6b4d1a541fd987ede33721
Piotrek <gosek33@gmail.com>
2023-03-02 20:46:44 +0100

    newfiles

---
+++ Assets/newcsfile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newcsfile 
+{
+    private int i = 10;
+    //this file will generate errors and warrnings!
+    void main()
+    {
+        if(true)
+            int a = 0;
+
+    }
+
+}
